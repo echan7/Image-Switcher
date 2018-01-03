09df9199c32e731401c5c0feb9d5b9c2a1483441
Eric Chan <echan7@wisc.edu>
2018-01-03 12:13:32 -0600

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,9 @@
+// popup.js
+
+function runSwitchjs() {
+  chrome.tabs.executeScript({
+    file: 'switch.js'
+  });
+}
+
+document.getElementById('clickme').addEventListener('click', runSwitchjs);
---
+++ switch.js
@@ -0,0 +1,6 @@
+// switch.js
+
+for (var i = 0; document.images.length; i++) {
+  // Change the url to anyting you want!
+  document.images[i].src = "https://ih0.redbubble.net/image.371913057.2805/flat,800x800,075,f.u2.jpg"
+};
